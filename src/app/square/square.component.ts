04bb1ce619d60dd80aa0d577c720f59b8fdefc0f
Yahya LAZREK <63449913+UUinc@users.noreply.github.com>
2024-11-20 20:12:01 +0100

    create square and board components

---
+++ src/app/square/square.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-square',
+  standalone: true,
+  imports: [CommonModule],
+  template: `<button>{{ value }}</button>`,
+  styles: ['button { width: 100%; height: 100%; font-size: 5em !important; }']
+})
+export class SquareComponent {
+  @Input() value: 'X' | 'O';
+
+  constructor() {
+    this.value = 'X';
+  }
+}
